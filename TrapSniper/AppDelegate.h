2fda5169d5e96faac50634710f24d929b813287c
Hai Phung <hai.phung@gmail.com>
2014-11-06 19:35:49 -0600

    Initial Commit

---
+++ TrapSniper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrapSniper
+//
+//  Created by Hai Phung on 11/6/14.
+//  Copyright (c) 2014 Hai Phung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TrapSniper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TrapSniper
+//
+//  Created by Hai Phung on 11/6/14.
+//  Copyright (c) 2014 Hai Phung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
